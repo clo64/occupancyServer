29e048b9ddf07fe46c289554a6defd0ca2c0ee20
Charles Owen <clo64@scarletmail.rutgers.edu>
2020-04-02 17:00:21 -0400

    Mongo Connectivity Added

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://chuck:myDBPass@personnel-gpoyg.mongodb.net/test?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-paerse');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//BodyParser
+app.use(bodyParser.json());
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 6000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
