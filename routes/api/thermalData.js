4c850a43066f734e80b02fee65bbd30322026587
Charles Owen <clo64@scarletmail.rutgers.edu>
2020-04-02 19:14:57 -0400

    Search thermal data via api by room# and floor#

--- routes/api/thermalData.js
+++ routes/api/thermalData.js
@@ -8,9 +8,24 @@ const Thermal = require('../../models/Thermal');
 //@desc  Show us thermal data for a floor and room
 //@access Public
 router.get('/', (req, res) => {
+    //Fetch all current thermal data
+    if(req.body.All == "1"){
     Thermal.find()
         .then(thermalData => res.json(thermalData))
-});
+    }
+    else{
+        Thermal.find({RoomNumber: req.body.RoomNumber, Floor: req.body.Floor})
+        .then(thermalData => 
+            {
+                if(!thermalData.length){
+                 res.json("No room, or floor match");
+                }
+                else{
+                 res.json(thermalData);
+                }
+            })
+        }
+    });
 
 //@route POST api/thermaldata
 //@desc  POST thermal data to mongo
