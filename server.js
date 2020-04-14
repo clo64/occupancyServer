3e370b3318abc7bb5bd686b14e7b24b36dc2d448
Charles Owen <clo64@scarletmail.rutgers.edu>
2020-04-14 17:59:44 -0400

    Fix file extension error

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
         cb(null, 'public/')
     },
     filename: function(req, file, cb) {
-        cb(null, 'thermal.jpeg')
+        cb(null, 'thermal.png')
     }
 });
 
