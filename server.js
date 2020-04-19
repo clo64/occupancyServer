742c2a96b485c0921e3cd599d06537e2fa0feefa
Charles Owen <clo64@scarletmail.rutgers.edu>
2020-04-19 18:10:13 -0400

    Heroku prep

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const Employee = require('./routes/api/employee');
 const Rooms = require('./routes/api/room');
 const Alert = require('./routes/api/alert');
 const multer = require('multer');
+const path = require('path');
 //Attempt to fix heroku static file access problem
 process.env.PWD = process.cwd()
 
@@ -51,6 +52,16 @@ app.use('/api/alert', Alert);
 app.post('/api/thermaldata/image', upload.single('image'), (req, res) => {
     res.json("Got an image");
 });
+
+//Serve statis assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
